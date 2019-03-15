1d05804d06ad218496eee8fe4c69f14499c5ff9d
Shania <shaniae.astagina@gmail.com>
2019-03-15 19:23:37 +0700

    modified:   README.md

--- soal1.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
- 
-i="1"
-for var in /home/sea/Documents/Sisop/Modul_1/nature/*.jpg
-do 
- base64 -d $var | xxd -r > /home/sea/Documents/Sisop/Modul_1/nature_republic/$i.jpg
- i=$(($i + 1))
-done
--- soal5.sh
+++
@@ -1,2 +0,0 @@
- 
-awk '/cron/ || /CRON/,!/sudo/' /var/log/syslog | awk 'NF < 13' >> /home/sea/Documents/Sisop/Modul_1/soal5.log
